d8022fdbde3c86c4a5b3819b88cb8b51c153df21
a <a>
2025-11-14 16:59:03 +0100

    interprete3

--- peliculasP2/peliculas_Accesible/src/pages/DetalleInterprete.jsx
+++ peliculasP2/peliculas_Accesible/src/pages/DetalleInterprete.jsx
@@ -1,7 +1,7 @@
 import { useParams, useNavigate } from "react-router-dom";
 import peliculas from "../data/peliculas";
 
-function DetalleInterprete(indice) {
+function DetalleInterprete() {
   
 }
 
---
+++ peliculasP2/peliculas_Accesible/src/pages/DetallePelicula.jsx
@@ -0,0 +1,8 @@
+import { useParams, useNavigate } from "react-router-dom";
+import peliculas from "../data/peliculas";
+
+function DetallePelicula() {
+  
+}
+
+export default DetallePelicula;
